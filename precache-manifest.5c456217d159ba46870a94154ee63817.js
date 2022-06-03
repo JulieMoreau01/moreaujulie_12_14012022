f471e05423c2e2e175cfc4acf470f05f227fe8c2
Julie Moreau <julie_bonnet_01@hotmail.com>
2022-06-03 15:27:01 +0200

    Updates

---
+++ precache-manifest.5c456217d159ba46870a94154ee63817.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddbc5cb1d3d2361c17876e0c402b68c",
+    "url": "/moreaujulie_12_14012022/index.html"
+  },
+  {
+    "revision": "59ab3fad8562faee0225",
+    "url": "/moreaujulie_12_14012022/static/css/main.5bce09cd.chunk.css"
+  },
+  {
+    "revision": "672278dbfc7443ee783b",
+    "url": "/moreaujulie_12_14012022/static/js/2.f16a855d.chunk.js"
+  },
+  {
+    "revision": "a9cdf003b4cc99ec0d4f8b988f410a04",
+    "url": "/moreaujulie_12_14012022/static/js/2.f16a855d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ab3fad8562faee0225",
+    "url": "/moreaujulie_12_14012022/static/js/main.8a7c95d1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/moreaujulie_12_14012022/static/js/main.8a7c95d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b72395c9d523e76e80e",
+    "url": "/moreaujulie_12_14012022/static/js/runtime-main.0bf0e679.js"
+  },
+  {
+    "revision": "fec6c31058b29d2e60bb37aa4381b787",
+    "url": "/moreaujulie_12_14012022/static/media/calories-icon.fec6c310.svg"
+  },
+  {
+    "revision": "b6097ceee0aad0961eb55673e820a37d",
+    "url": "/moreaujulie_12_14012022/static/media/carbs-icon.b6097cee.svg"
+  },
+  {
+    "revision": "6e4294b2b9952cc7139c0c2d116d8173",
+    "url": "/moreaujulie_12_14012022/static/media/fat-icon.6e4294b2.svg"
+  },
+  {
+    "revision": "606a97bccd26352696a5871e4136f8d5",
+    "url": "/moreaujulie_12_14012022/static/media/logo.606a97bc.svg"
+  },
+  {
+    "revision": "15bda523f68472df19891488fa94059a",
+    "url": "/moreaujulie_12_14012022/static/media/navlicon.15bda523.svg"
+  },
+  {
+    "revision": "f60d29ede793b08c9b038b3a01f5b3c9",
+    "url": "/moreaujulie_12_14012022/static/media/navlicon2.f60d29ed.svg"
+  },
+  {
+    "revision": "669100783d5fb1f515c93942241dad9e",
+    "url": "/moreaujulie_12_14012022/static/media/navlicon3.66910078.svg"
+  },
+  {
+    "revision": "3a52796f0ceeee12e2a12c0f288a9213",
+    "url": "/moreaujulie_12_14012022/static/media/navlicon4.3a52796f.svg"
+  },
+  {
+    "revision": "882174f3c9a6010aebff6616ab0c74f3",
+    "url": "/moreaujulie_12_14012022/static/media/protein-icon.882174f3.svg"
+  }
+]);
